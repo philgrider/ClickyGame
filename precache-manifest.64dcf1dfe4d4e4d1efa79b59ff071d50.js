432fad7f3cb35115f0594ffbda7a660224492034
PHILGRIDER <39570510+philgrider@users.noreply.github.com>
2019-01-13 14:22:41 -0600

    Updates

---
+++ precache-manifest.64dcf1dfe4d4e4d1efa79b59ff071d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "389ad7d6f82d875599c6a6eaeeaed98a",
+    "url": "/ClickyGame/static/media/react.389ad7d6.svg"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "11135912b9a5bd402ba6",
+    "url": "/ClickyGame/static/js/main.11135912.chunk.js"
+  },
+  {
+    "revision": "442048274d786335d7bb",
+    "url": "/ClickyGame/static/js/1.44204827.chunk.js"
+  },
+  {
+    "revision": "11135912b9a5bd402ba6",
+    "url": "/ClickyGame/static/css/main.002ce296.chunk.css"
+  },
+  {
+    "revision": "e0bb7e937e787a7ed2bc4602742b22be",
+    "url": "/ClickyGame/index.html"
+  }
+];
